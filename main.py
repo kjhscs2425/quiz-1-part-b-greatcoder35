8b13a8d542726df234807ce94a8e3adc8703c3d4
greatcoder35 <jnash2027@kehillahstudent.org>
2024-10-23 10:41:21 -0700

    finished quiz

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import turtle
-
+from turtle import *
 # YOUR CODE HERE
-
-turtle.exitonclick()
+for i in range(4):
+    forward(100)
+    right(90)
+forward(50)
+right(45)
+for i in range(4):
+    forward(71)
+    right(90)
+exitonclick()
